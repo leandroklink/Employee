233ddc8ce82345ec2d481ecf055f0c34d084b318
Leandro <leandroklink13@gmail.com>
2026-01-05 21:21:18 -0300

    algumas adições

--- Employee.java
+++ Employee.java
@@ -11,16 +11,21 @@ package date;
 public class Employee {
     private String firstName;
     private String lastName;
-    private Date birthDate;
-    private Date hireDate;
+    private static int count =0;
     
-    public Employee(String first, String last, Date dateOfBirth, Date dateOfHire){
+    public Employee(String first, String last){
         firstName = first;
         lastName = last;
-        birthDate = dateOfBirth;
-        hireDate = dateOfHire;
+        ++count;
+        System.out.printf("Employee constructor: %s %s; count = %d\n", firstName, lastName, count);
     }
-    public String toString(){
-        return String.format("%s, %s Hired: %s BirthDay: %s", lastName, firstName, hireDate, birthDate);
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public static int getCount(){
+        return count;
     }
 }
